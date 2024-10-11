cf83ffa481c71dac80dcd7c8160424236f778c6d
LuisDamas <91203176+LuisDamas@users.noreply.github.com>
2024-10-11 12:02:15 +0100

    Add files via upload

---
+++ src/main/java/pt/pa/Main.java
@@ -0,0 +1,17 @@
+package pt.pa;
+
+/**
+ *
+ * @author amfs
+ */
+public class Main {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+
+
+}
